f9c978adcb1175af28d5176a69c8a88c585b42ed
Stefania Stefansdottir <sstefansdottir88@gmail.com>
2020-01-29 23:46:38 -0600

    second commit

---
+++ script.js
@@ -0,0 +1,66 @@
+
+
+
+const questionContainer = document.getElementById("questions");
+const resultContainer = document.getElementById("results");
+const submitButtonA = document.getElementById("buttonA");
+const submitButtonB = document.getElementById("buttonB");
+const submitButtonC = document.getElementById("buttonC");
+const submitButtonD = document.getElementById("buttonD");
+
+
+// set the variables
+
+// create a start button for the quiz
+        //when the start button is clicked the questionOne will appear and a timer starts.
+//
+counter++
+counger--
+var score = 0;
+
+var questions = [
+    {
+        question: "What is the color of the sky?",
+        answers: ["blue", "red", "green", "yellow"]
+    
+    },
+    {
+        question: "Who of the four is not a Beatle?",
+        answers: ["George Harrison", "David Hasselhoff", "John Lennon", "Ringo Starr"]
+    },
+    {
+        question: "Which one is not a Super-Hero?",
+        answers: ["Sponge Bob", "Wonder Woman", "Superman", "Captain Marvel"]
+    },
+    {
+        question: "What nationality is Mo Salah?",
+        answers: ["Martian", "Italian", "Egyptian", "Norwegian"]
+    },
+    {
+        question: "What is the capital city of Iceland?",
+        answers: ["Rome", "Smurflandia", "Cobenhagen", "Reylkavik"]
+    },
+    {
+        question: "Name the volcano that erupted in Iceland in 2010?",
+        answers: ["Snaefellsjokull", "Lakagigar", "Eyjafjallajokull", "Hekla"]
+    }
+
+
+] 
+
+
+
+for (let i = 0; i < questions.length; i++) {
+    var response = answers.length[i]
+    
+}
+
+
+
+
+
+
+
+
+
+questionsAndAnswers[1].question
